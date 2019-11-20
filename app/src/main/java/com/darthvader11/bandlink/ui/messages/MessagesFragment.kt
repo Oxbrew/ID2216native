88cc9012bff4d10a562a617b3f2e3a077f4e3d56
Calin Capitanu <calincapitanu@gmail.com>
2019-11-20 22:07:23 +0100

    moved post to search fragment

--- app/src/main/java/com/darthvader11/bandlink/ui/messages/MessagesFragment.kt
+++ app/src/main/java/com/darthvader11/bandlink/ui/messages/MessagesFragment.kt
@@ -1,21 +1,14 @@
 package com.darthvader11.bandlink.ui.messages
-import android.content.Intent
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import androidx.lifecycle.ViewModelProviders
 import com.darthvader11.bandlink.R
-import com.darthvader11.bandlink.ui.comment.CommentsFragment
 
-class MessagesFragment : Fragment(), View.OnClickListener {
+class MessagesFragment : Fragment() {
 
-    private lateinit var messagesViewModel: MessagesViewModel
 
     override fun onCreateView(
 
@@ -26,39 +19,11 @@ class MessagesFragment : Fragment(), View.OnClickListener {
 
         val root = inflater.inflate(R.layout.fragment_messages, container, false)
 
-        val btnComment :Button =  root.findViewById(R.id.btnComment)
-        btnComment.setOnClickListener(this)
-        val btnShare :Button = root.findViewById(R.id.btnShare)
-        btnShare.setOnClickListener(this)
 
         return root
 
 
     }
 
-    override fun onClick(v: View?) {
-        when (v?.id){
-            R.id.btnComment -> {
-                Toast.makeText(context, "DIZ WORKS", Toast.LENGTH_SHORT).show()
-                val manager: FragmentManager? = fragmentManager
-                val transaction: FragmentTransaction? = manager?.beginTransaction()
-                transaction?.replace(R.id.nav_host_fragment, CommentsFragment() , CommentsFragment::class.java.simpleName  )
-                transaction?.addToBackStack(null)
-                transaction?.commit()
-            }
-            R.id.btnShare -> {
-                val intent = Intent()
-                val message: String = "calincapitanu.com/Post.html"
-                intent.action = Intent.ACTION_SEND
-                intent.putExtra(Intent.EXTRA_TEXT, message)
-                intent.type = "text/plain"
 
-                startActivity(Intent.createChooser(intent, "Share to: "))
-            }
-
-
-
-        }
-
-    }
 }
--- app/src/main/java/com/darthvader11/bandlink/ui/search/SearchFragment.kt
+++ app/src/main/java/com/darthvader11/bandlink/ui/search/SearchFragment.kt
@@ -1,31 +1,64 @@
 package com.darthvader11.bandlink.ui.search
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.darthvader11.bandlink.R
+import com.darthvader11.bandlink.ui.comment.CommentsFragment
 
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(), View.OnClickListener {
 
-    private lateinit var searchViewModel: SearchViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        searchViewModel =
-                ViewModelProviders.of(this).get(SearchViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_search, container, false)
-        val textView: TextView = root.findViewById(R.id.text_search)
-        searchViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
+        val btnComment : Button =  root.findViewById(R.id.btnComment)
+        btnComment.setOnClickListener(this)
+        val btnShare : Button = root.findViewById(R.id.btnShare)
+        btnShare.setOnClickListener(this)
+
+
         return root
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id){
+            R.id.btnComment -> {
+                Toast.makeText(context, "DIZ WORKS", Toast.LENGTH_SHORT).show()
+                val manager: FragmentManager? = fragmentManager
+                val transaction: FragmentTransaction? = manager?.beginTransaction()
+                transaction?.replace(R.id.nav_host_fragment, CommentsFragment() , CommentsFragment::class.java.simpleName  )
+                transaction?.addToBackStack(null)
+                transaction?.commit()
+            }
+            R.id.btnShare -> {
+                val intent = Intent()
+                val message: String = "calincapitanu.com/Post.html"
+                intent.action = Intent.ACTION_SEND
+                intent.putExtra(Intent.EXTRA_TEXT, message)
+                intent.type = "text/plain"
+
+                startActivity(Intent.createChooser(intent, "Share to: "))
+            }
+
+
+
+        }
+
     }
 }
