957bb09e09e90d1fcfcd7239d0e1048568634cbc
chaos7777 <47792925+chaos7777@users.noreply.github.com>
2019-11-23 18:08:44 +0100

    Made profile scrollabe,added post button to see all posts

--- app/src/main/java/com/darthvader11/bandlink/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/darthvader11/bandlink/ui/profile/ProfileFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.darthvader11.bandlink.R
+import com.darthvader11.bandlink.ui.comment.PostlistFragment
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment(), View.OnClickListener {
@@ -22,17 +26,33 @@ class ProfileFragment : Fragment(), View.OnClickListener {
 
         val submitButton: Button = root.findViewById(R.id.submitButton)
         submitButton.setOnClickListener(this)
+        val backButton : ImageView = root.findViewById(R.id.backButton)
+        backButton.setOnClickListener(this)
+        val btnPosts: Button = root.findViewById(R.id.btnPosts)
+        btnPosts.setOnClickListener(this)
         return root
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
+            R.id.backButton -> {
+                fragmentManager?.popBackStack()
+            }
             R.id.submitButton -> {
 
                 //   val totalStarts = "total Stars :" + ratingBar.numStars
                 val rating = "Rating :" +  ratingBar.rating
                 Toast.makeText(context, rating, Toast.LENGTH_SHORT).show()
 
+            }
+
+            R.id.btnPosts -> {
+                Toast.makeText(context, "Posts", Toast.LENGTH_SHORT).show()
+                val manager: FragmentManager? = fragmentManager
+                val transaction: FragmentTransaction? = manager?.beginTransaction()
+                transaction?.replace(R.id.nav_host_fragment, PostlistFragment() , PostlistFragment::class.java.simpleName  )
+                transaction?.addToBackStack(null)
+                transaction?.commit()
             }
         }
     }
