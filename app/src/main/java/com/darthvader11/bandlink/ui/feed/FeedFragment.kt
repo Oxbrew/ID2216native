d69dafadf82c741dff2e89e6ef01437bf58e0ceb
Calin Capitanu <calincapitanu@gmail.com>
2019-11-24 15:40:49 +0100

    feed done

--- app/src/main/java/com/darthvader11/bandlink/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/darthvader11/bandlink/ui/feed/FeedFragment.kt
@@ -6,13 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.darthvader11.bandlink.R
+import com.darthvader11.bandlink.adaptors.CommentsAdapter
+import com.darthvader11.bandlink.adaptors.FeedAdapter
+import com.darthvader11.bandlink.models.Supplier
+import com.darthvader11.bandlink.models.feedSupplier
 import com.darthvader11.bandlink.ui.newpost.NewpostFragment
 
 class FeedFragment : Fragment(), View.OnClickListener {
+
+    lateinit var adapter: FeedAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,10 +29,29 @@ class FeedFragment : Fragment(), View.OnClickListener {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_feed, container, false)
-        val createPost: Button = root.findViewById(R.id.createPost)
+        val createPost: AppCompatImageButton = root.findViewById(R.id.createPost)
         createPost.setOnClickListener(this)
+
+        val recyclerFeed: RecyclerView = root.findViewById(R.id.recyclerFeed)
+        setupRecyclerView(recyclerFeed)
+
         return root
     }
+
+
+    private fun setupRecyclerView(recyclerFeed: RecyclerView){
+
+
+        val layoutManager = LinearLayoutManager(context)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerFeed.layoutManager = layoutManager
+        adapter = FeedAdapter(context!!, feedSupplier.feedContent)
+        recyclerFeed.adapter = adapter
+
+
+
+    }
+
 
     override fun onClick(v: View?) {
         when (v?.id) {
